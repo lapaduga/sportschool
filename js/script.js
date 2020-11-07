f080a9024b5095fbd4abab9be1ff9ead540df8b9
lapaduga <deniskotov29042015@gmail.com>
2020-11-07 13:48:25 +0500

    works!!!

--- js/script.js
+++ js/script.js
@@ -30,6 +30,18 @@ $(document).ready(function () {
 		}
 	} else {
 		body.classList.add('mouse');
+		let arrow = document.querySelectorAll('.arrow');
+		for (i = 0; i < arrow.length; i++) {
+			let thisLink = arrow[i].previousElementSibling;
+			let subMenu = arrow[i].nextElementSibling;
+			let thisArrow = arrow[i];
+
+			thisLink.classList.add('parent');
+			arrow[i].addEventListener('click', function () {
+				subMenu.classList.toggle('open');
+				thisArrow.classList.toggle('active');
+			});
+		}
 	}
 
 
