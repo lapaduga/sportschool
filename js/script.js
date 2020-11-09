63d1344ba78de85d7a4d1dbca0d39a4cdcaa0d57
lapaduga <deniskotov29042015@gmail.com>
2020-11-10 00:44:22 +0500

    main page done

--- js/script.js
+++ js/script.js
@@ -117,4 +117,32 @@ $(document).ready(function () {
 			}
 		]
 	});
+
+
+	//news__slider
+	$('.news__slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		dots: true,
+		responsive:[
+			{
+				breakpoint: 1601,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 1101,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+		]
+	});
 });
